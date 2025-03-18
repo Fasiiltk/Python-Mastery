d3ab0f097a709679fdf8bcc983a99c4455f5ee03
FASIL RAHMAN TK <fasilpersonal07@gmail.com>
2025-03-18 20:21:59 +0530

    alarm_clock.py

--- alarm_clock.py
+++ alarm_clock.py
@@ -2,7 +2,7 @@
 import time
 from datetime import datetime
 
-alarm_time = input("Enter alarm time (HH:MM): ")
+alarm_time = input("Enter alarm time (HH:MM): ")  
 while True: 
     current_time = datetime.now().strftime("%H:%M")
     if current_time == alarm_time:
